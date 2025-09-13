fca30d930379e8d4f57cc9ee8d0b616ed583cb84
w <none>
2025-09-13 14:08:11 -0700

    workbench: indentation fix

--- projects/workbench/init/workbench/index.js
+++ projects/workbench/init/workbench/index.js
@@ -1,15 +1,15 @@
 // ~ //
 const stuff = new /** @type {import('fastly:kv-store')} */ (builtinModules.fastlyKVStore).KVStore('stuff');
 switch (innerPath) {
-    case '/':
-        const entry = await stuff.get('workbench/index.html');
-        return new Response(entry.body, {headers: {
-            'Content-Type': 'text/html; charset=utf-8',
-            'Access-Control-Allow-Origin': '*',
-	        'Cross-Origin-Resource-Policy': 'cross-origin',
-            'Cross-Origin-Embedder-Policy': 'credentialless',
-            'Cross-Origin-Opener-Policy': 'same-origin',
-        }});
-    default:
-        return new Response('not found\n', {status: 404});
+	case '/':
+		const entry = await stuff.get('workbench/index.html');
+		return new Response(entry.body, {headers: {
+			'Content-Type': 'text/html; charset=utf-8',
+			'Access-Control-Allow-Origin': '*',
+			'Cross-Origin-Resource-Policy': 'cross-origin',
+			'Cross-Origin-Embedder-Policy': 'credentialless',
+			'Cross-Origin-Opener-Policy': 'same-origin',
+		}});
+	default:
+		return new Response('not found\n', {status: 404});
 }
